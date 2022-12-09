952431300235cedff36a5cdf274fc172fc8b7251
rajkumark <bravoraj2906@gmail.com>
2022-12-09 12:58:32 +0530

    RAJKUMAR FRIDAY-TEST SQL

--- src/main/java/org/example/Sample.java
+++ src/main/java/org/example/Sample.java
@@ -1,29 +1,9 @@
 package org.example;
 
-import java.util.Objects;
-
-class account {
-    private final int accountNumber;
-    private final String accountName;
-    private final char accountBalance;
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
-    public String getAccountName() {
-        return accountName;
-    }
-
-    public char getAccountBalance() {
-        return accountBalance;
-    }
-
-    public account(int accountNumber, String accountName, char accountBalance){
-        this.accountNumber = accountNumber;
-        this.accountName = accountName;
-        this.accountBalance = accountBalance;
-
-
+public class Solution{
+    public static void   main(String[] args){
+        short x =   10;
+        x =  x * 5;
+        System.out.print(x);
     }
 }
---
+++ src/main/java/training/thread/Producer.java
@@ -0,0 +1,8 @@
+package training.thread;
+
+public class Producer implements Runnable{
+    @Override
+    public void run(){
+
+    }
+}
